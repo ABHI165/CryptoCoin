e50b10908d47f4a9e783ab661dd051ea97549439
Abhishek Agarwal <abhiagarwal16052000@gmail.com>
2022-02-19 15:55:03 +0530

    Initial Commit

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by Abhishek Agarwal on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
